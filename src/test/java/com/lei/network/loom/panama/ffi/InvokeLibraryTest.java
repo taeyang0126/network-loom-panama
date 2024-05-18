7a7d3b8151f53ffefca8c1ad9dc0334c37a82f36
lei <17674030991@163.com>
2024-05-18 19:04:57 +0800

    [ffi] 使用project panama完成调用c函数

--- src/test/java/com/lei/network/loom/panama/ffi/InvokeLibraryTest.java
+++ src/test/java/com/lei/network/loom/panama/ffi/InvokeLibraryTest.java
@@ -35,6 +35,10 @@ public class InvokeLibraryTest {
         MethodHandle handle = Linker.nativeLinker().downcallHandle(func, FunctionDescriptor.ofVoid(ValueLayout.ADDRESS));
 
         // 使用 project Panama 提供的堆外内存机制，Arena 对象维护着一块堆外内存的生命周期，可以通过Arena来进行不同内存布局的堆外内存的分配，当Arena对象被关闭时，通过Arena分配的所有内存也会被释放
+        // 推荐使用 try-with-resources 形式维护 Arena 申请的堆外内存，避免出现内存泄漏
+        // Arena.ofAuto() 不需要显式管理内存释放，将该块堆外内存交给gc管理
+        // Arena.ofConfined() 申请的内存，只能由申请内存的线程进行访问，JVM内部会对调用方进行严格的检查，不允许出现跨线程访问的情况
+        // Arena.ofShared() 作用域申请的内存可以进行跨线程访问，但不在JVM内存模型的规范中，需要开发者手动去添加内存屏障，或使用其他方式加锁，来保证线程可见性和一致性
         try (Arena arena = Arena.ofConfined()) {
             // 分配了一个 UTF8格式的，以C风格'\0'作为结尾的字符串，并返回一个指向该字符串的指针
             // c 语言中的指针在java中表现为 MemorySegment 对象
@@ -44,5 +48,7 @@ public class InvokeLibraryTest {
         } catch (Throwable e) {
             throw new RuntimeException(e);
         }
+
+        // 如果需要采用共享堆外内存的方式来进行通信，那么可以手动的根据场景插入VarHandle.fullFence()等内存屏障，或者是使用pthread库中提供的mutex对直接内存进行加锁
     }
 }
